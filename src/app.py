15a8204b3ce1405bd45db493ef2cc7481c67353b
nelson <nelson.antunes994@gmail.com>
2023-06-15 13:20:27 +0200

    feat: add pytorch model skeleton and predictions

--- src/app.py
+++ src/app.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 import gradio as gr
 import numpy as np
-
+from model import sketch_recognizer
+import torch
+import torchvision.transforms as T
+from PIL import Image
+from pathlib import Path
 
 # pylint: disable=W0613
+
+LABELS = Path("labels.txt").read_text().splitlines()
 
 
 def sketch_recognition(input_sketch: np.ndarray) -> dict:
@@ -17,20 +23,24 @@ def sketch_recognition(input_sketch: np.ndarray) -> dict:
         confidences: the top five predictions, as a dictionary whose keys are class labels
                      and whose values are confidence probabilities
     """
-    confidences = {
-        "laptop": 0.1,
-        "cup of coffee": 0.3,
-        "ghost": 0.2,
-        "gift": 0.2,
-        "other": 0.2,
-    }
+    input_sketch = Image.fromarray(input_sketch).resize((28, 28))
+    input_sketch = T.ToTensor()(input_sketch)
+
+    with torch.no_grad():
+        out = sketch_recognizer(input_sketch.unsqueeze(0))
+
+    probabilities = torch.nn.functional.softmax(out[0], dim=0)
+    values, indices = torch.topk(probabilities, k=out.shape[-1])
+
+    confidences = {LABELS[i]: v.item() for i, v in zip(indices, values)}
+
     return confidences
 
 
 ARTICLE_OPENING = """
 <center> 
-The bot was trained to recognize your sketch of a laptop, a cup of coffee, a ghost or a gift. Have fun drawing and 
-try to fool it!
+Ghostee the ghost doesn't know hunger, but he has been trained to recognize your sketch of what he thinks
+is the best food: pizza, hamburger, hot-dog, donut, or... broccoli! Have fun drawing and helping Ghostee.
 </center> 
 """
 
@@ -40,7 +50,9 @@ Check out my <a href="https://www.nelsonantunes.com">blog</a> where I provide mo
 </center>
 """
 
-sketch_pad = gr.Sketchpad(shape=(512, 512), brush_radius=24, label="Draw on sketch pad")
+sketch_pad = gr.Sketchpad(
+    shape=(1024, 1024), brush_radius=24, label="Draw on sketch pad"
+)
 
 demo = gr.Interface(
     fn=sketch_recognition,
@@ -50,7 +62,7 @@ demo = gr.Interface(
     description=ARTICLE_OPENING,
     theme=gr.themes.Soft(),
     article=ARTICLE_ENDING,
-    flagging_options=["👍 Correct", "👎 Incorrect"],
+    flagging_options=["👍", "👎"],
 )
 
 demo.queue()
